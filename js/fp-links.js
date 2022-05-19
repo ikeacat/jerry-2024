ca0c12c649937a20717d9fc132688332497db9ca
ikeacat <mfryk268@gmail.com>
2022-05-19 00:10:30 -0500

    jiafei lol

---
+++ js/fp-links.js
@@ -0,0 +1,15 @@
+function shop() {
+    window.location = "https://www.amazon.com/Biden-Sticker-Bidens-Sarcasm-Inches/dp/B09GFPB9W3/ref=asc_df_B09GFPB9W3/";
+}
+
+function donate() {
+    window.location = "subpage/donate.html"
+}
+
+function issues() {
+    window.location = "subpage/issues.html"
+}
+
+function about() {
+    window.location = "subpage/about.html"
+}
